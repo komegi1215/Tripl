8f2ea17f0f4b4bdefb01ab107a23617c79bb7c79
syrene bartolome <syrene.h.bartolome@icloud.com>
2024-01-23 20:02:58 +0900

    created trips and bookings controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/edit'
+  get 'bookings/update'
+  get 'bookings/destroy'
+  get 'trips/index'
+  get 'trips/show'
+  get 'trips/new'
+  get 'trips/create'
+  get 'trips/edit'
+  get 'trips/update'
+  get 'trips/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
