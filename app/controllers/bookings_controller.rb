c9039355bd399c73c1404ada46e0aa021423fdae
Justin Limlengco <jjmlimlengco@gmail.com>
2024-01-25 20:39:46 +0900

    Removed unnecessary RCV for user bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -25,8 +25,4 @@ class BookingsController < ApplicationController
     @booking.destroy
     redirect_to trips_path(@booking.trip), status: :see_other
   end
-
-  def user_bookings
-    @user = User.find(params[:user_id])
-  end
 end
